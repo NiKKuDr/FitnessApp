ac364ee041b876fc3391559f3cddf1fb2f6981b5
Nikolai Kudriashov <nikolaikudriashov@yahoo.com>
2022-03-29 04:06:27 -0400

    First Commit

---
+++ AppBusinessLogic/Model/Gender.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppBusinessLogic.Model
+{
+    /// <summary>
+    /// Gender
+    /// </summary>
+    public class Gender
+    {
+        /// <summary>
+        /// Name
+        /// </summary>
+        public string Name { get; }
+        /// <summary>
+        /// Creating a new gender
+        /// </summary>
+        /// <param name="name"> Name of Gender </param>
+        //for gender I create a constructor
+        public Gender(string name)
+        {
+            // check input arguments(conditions)
+            if(string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("The gender name cannot be empty or null", nameof(name));
+            }
+
+            // Making an assignment
+            Name = name;
+        }
+        // redefine method to string
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+    }
+}
---
+++ AppBusinessLogic/Model/User.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppBusinessLogic.Model
+{
+    class User
+    {
+        // I will only get; this property (I can only set the name once when creating an instance of this class)
+        // get; - readonly property that allows to set it once in the constructor. In the future, I will not change it.
+        public string Name { get; }
+        // there is no way to change the gender and name in the future
+        public Gender Gender { get;}
+        public DateTime BirthDate { get; }
+        // The user's weight will change
+        public double Weight { get; set; }
+        public double Height { get; set; }
+
+        // creating a constructor for the user
+        public User(string name, Gender gender, DateTime birthDate, double weight, double height)
+        {
+            // validation of input parameters
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Name cannot be empty or null", nameof(name));
+            }
+            if (gender == null)
+            {
+                throw new ArgumentNullException("Gender cannot be null", nameof(gender));
+            }
+            if (birthDate < DateTime.Parse("01.01.1900"))
+            {
+            }
+            if (weight <= 0)
+            {
+            }
+            if (height <= 0)
+            {
+
+            }
+
+            Name = name;
+            Gender = gender;
+            BirthDate = birthDate;
+            Weight = weight;
+            Height = height;
+        }
+
+
+
+    }
+}
